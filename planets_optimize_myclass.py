53edb1f4a8cd648238588037928caa3f0bbd54b9
stwuiwmoc <stwuiwmoc5929@gmail.com>
2021-12-28 17:36:37 +0900

    トルクによる変形を見る部分と、測定データを円環パスに起こす部分を別ファイルに移動

--- planets_optimize_myclass.py
+++ planets_optimize_myclass.py
@@ -68,14 +68,6 @@ def make_remaining_matrix(matrix, ignore_zernike_number_list):
     remaining_matrix = np.delete(arr=matrix, obj=idx_array, axis=0)
     return remaining_matrix
 
-def make_full_torque_value_array(torque_number_list, torque_value_aray):
-    full_value_array = np.zeros(36)
-    idx_array = np.array(torque_number_list) -1
-    for i in range(len(idx_array)):
-        idx = idx_array[i]
-        full_value_array[idx] = torque_value_aray[i]
-    return full_value_array
-
 
 class Constants:
     
@@ -401,55 +393,3 @@ class TorqueToZernike:
         ax.hlines(0, xmin=1, xmax=12, color ="darkgray")
         
         return ax
-
-    
-if __name__ == "__main__":
-    
-    CONSTS = Constants(physical_radius=925e-3, 
-                       ignore_radius=25e-3,
-                       pixel_number=256,
-                       zernike_max_degree = 10)
-    
-    """
-    # for parameter study
-    original_torque_value_array = make_full_torque_value_array([1,7,13,19,25,31],
-                                                               [5,-5,5,-5,5,-5])
-    original_torque_value_array = make_full_torque_value_array([2,8,14,20,26,32],
-                                                               [5,-5,5,-5,5,-5])
-    original_torque_value_array = make_full_torque_value_array([4,10,16,22,28,34],
-                                                               [5,-5,5,-5,5,-5])
-    original_torque_value_array = make_full_torque_value_array([5,11,17,23,29,35],
-                                                               [5,-5,5,-5,5,-5])    
-    original_torque_value_array = make_full_torque_value_array([6,12,18,24,30,36],
-                                                               [5,-5,5,-5,5,-5])
-    
-    original_torque_value_array = make_full_torque_value_array([1,7,13,19,25,31, 3,9,15,21,27,33, 4,10,16,22,28,34, 5,11,17,23,29,35, 6,12,18,24,30,36],
-                                                               [-5,5,-5,5,-5,5, -5,5,-5,5,-5,5, -5,5,-5,5,-5,5, -5,5,-5,5,-5,5, 5,-5,5,-5,5,-5])
-    """
-
-
-    original_torque_value_array = make_full_torque_value_array([3,9,15,21,27,33],
-                                                               [5,-5,5,-5,5,-5])
-    wh_deformed_zernike = TorqueToZernike(constants=CONSTS,
-                                          torque_value_array=original_torque_value_array,
-                                          restructed_torque_value=5,
-                                          ignore_zernike_number_list=[1,2,3,4,5,6])
-    
-    wh_deformed_surface = ZernikeToSurface(constants=CONSTS,
-                                           zernike_number_list=wh_deformed_zernike.remaining_zernike_number_list,
-                                           zernike_value_array=wh_deformed_zernike.remaining_reproducted_zernike_value_array)
-    
-    fig = plt.figure(figsize=(15,7))
-    gs = fig.add_gridspec(2,3)
-    wh_deformed_zernike.make_torque_plot(figure=fig, position=gs[0,2])
-    wh_deformed_surface.make_image_plot(figure=fig, position=gs[0:2,0:2])
-    wh_deformed_surface.make_circle_path_plot(figure=fig, position=gs[1,2])
-    fig.tight_layout()
-    
-    
-    diff = StitchedCsvToSurface(constants=CONSTS,
-                                original_stitched_csv_fpath="mkfolder/stitch2mesh/zer10_1215xm1301214ym870-510cir.v4.22.hei_dense.csv", 
-                                deformed_stitched_csv_fpath="mkfolder/stitch2mesh/zer10_1215xm1301216ym870-510cir.v4.21.hei_dense.csv")
-    
-    diff.make_image_plot()
-    diff.make_circle_path_plot()
