be663ef8a9c31161abf221efc57ef4c1c0dda767
stwuiwmoc <stwuiwmoc5929@gmail.com>
2021-12-29 11:11:11 +0900

    csv一つだけの読み込み、exelisデータの読み込みができるようになったので、読み込み検証用の部分は削除

--- pom_wh_deform_measurement_comparison.py
+++ pom_wh_deform_measurement_comparison.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     CONSTS = pom.Constants(physical_radius=925e-3, 
                            ignore_radius=25e-3,
                            pixel_number=256,
-                           zernike_max_degree = 10)
+                           zernike_max_degree=10)
     
     
     diff = pom.StitchedCsvToSurface(constants=CONSTS,
@@ -21,15 +21,3 @@ if __name__ == "__main__":
     
     diff.make_image_plot()
     diff.make_circle_path_plot()
-    
-    non_diff_test = pom.StitchedCsvToSurface(constants=CONSTS, 
-                                             original_stitched_csv_fpath="mkfolder/stitch2mesh/zer10_1215xm1301214ym870-510cir.v4.22.hei_dense.csv", 
-                                             None_or_deformed_stitched_csv_fpath=None)
-    
-    non_diff_test.make_image_plot()
-    
-    exelis_read_test = pom.StitchedCsvToSurface(constants=CONSTS, 
-                                                original_stitched_csv_fpath="mkfolder/exelis_rawdata_edit/exelis_reshaped.csv", 
-                                                None_or_deformed_stitched_csv_fpath=None)
-    
-    exelis_read_test.make_image_plot(figure=plt.figure())
