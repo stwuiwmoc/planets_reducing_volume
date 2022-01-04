d10cccb04d1e3bf5bf044d926816f119eb3e1b74
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-01-04 17:48:27 +0900

    constantsクラスにoffset_height_percentを入れ、Surfaceと派生クラスではself.constsからoffset_height_percentを引き出すようにした

--- pom_test_class_oapminimize.py
+++ pom_test_class_oapminimize.py
@@ -15,7 +15,8 @@ if __name__ == "__main__":
     CONSTS = pom.Constants(physical_radius=925e-3, 
                            ignore_radius=25e-3,
                            pixel_number=1024,
-                           zernike_max_degree=10)
+                           zernike_max_degree=10,
+                           offset_height_percent=2)
     
     OAP = pom.OapConstants(ideal_radius_of_curvature=8667e-3, 
                            ideal_off_axis_distance=1800e-3, 
@@ -26,31 +27,32 @@ if __name__ == "__main__":
   
     exelis = pom.StitchedCsvToSurface(constants=CONSTS, 
                                       original_stitched_csv_fpath="mkfolder/exelis_rawdata_edit/exelis_reshaped.csv",
-                                      deformed_stitched_csv_fpath="",
-                                      offset_height_percent=2)
+                                      deformed_stitched_csv_fpath="")
+                                      
     
     filtered_exelis = pom.FilteredSurface(constants=CONSTS, 
                                           inputed_surface=exelis.surface, 
-                                          filter_parameter=100,
-                                          offset_height_percent=2)
+                                          filter_parameter=100)
+                                          
     
     ideal_oap = pom.OapSurface(constants=CONSTS, 
                                radius_of_curvature=OAP.ideal_radius_of_curvature, 
                                off_axis_distance=OAP.ideal_off_axis_distance, 
-                               clocking_angle_rad=OAP.ideal_clocking_angle_rad,
-                               offset_height_percent=2)
+                               clocking_angle_rad=OAP.ideal_clocking_angle_rad)
+                               
     
     oap_minimize = pom.OapMinimize(constants=CONSTS, 
                                    oap_constants=OAP, 
-                                   inputed_surface=filtered_exelis.surface, 
-                                   offset_height_percent=2)
+                                   inputed_surface=filtered_exelis.surface)
+                                   
     
     optimized_oap = pom.OapSurface(constants=CONSTS, 
                                    radius_of_curvature=oap_minimize.result_parameters[0], 
                                    off_axis_distance=oap_minimize.result_parameters[1], 
                                    clocking_angle_rad=oap_minimize.result_parameters[2])
+                                   
     
     oap_optimized_exelis = pom.Surface(constants=CONSTS, 
-                                       surface=filtered_exelis.surface+ideal_oap.surface-optimized_oap.surface,
-                                       offset_height_percent=2)
+                                       surface=filtered_exelis.surface+ideal_oap.surface-optimized_oap.surface)
+                                       
     
