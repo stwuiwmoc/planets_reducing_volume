a5e32336104bddde5553ce8bb6643197a76d11cf
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-01-30 22:38:09 +0900

    変数名をパターン3に拘らなくした

--- pom_test_circle_path_measurement_reading.py
+++ pom_test_circle_path_measurement_reading.py
@@ -13,29 +13,29 @@ if __name__ == "__main__":
                            zernike_max_degree=10,
                            offset_height_percent=2)
 
-    input_filename0 = "../sag_integration_code/" + "mkfolder/psm_test_kagi_data_integration/0117e_height.csv"
-    input_filename3 = "../sag_integration_code/" + "mkfolder/psm_test_kagi_data_integration/0117h_height.csv"
+    input_filename_0 = "../sag_integration_code/" + "mkfolder/psm_test_kagi_data_integration/0117e_height.csv"
+    input_filename_n = "../sag_integration_code/" + "mkfolder/psm_test_kagi_data_integration/0117h_height.csv"
 
-    mes03 = pom.CirclePathMeasurementReading(Constants=CONSTS,
-                                             original_csv_fpath=input_filename0,
-                                             deformed_csv_fpath=input_filename3)
+    mes0n = pom.CirclePathMeasurementReading(Constants=CONSTS,
+                                             original_csv_fpath=input_filename_0,
+                                             deformed_csv_fpath=input_filename_n)
 
-    res03 = pom.CirclePathZernikeFitting(Constants=CONSTS,
+    res0n = pom.CirclePathZernikeFitting(Constants=CONSTS,
                                          circle_path_radius=870e-3,
-                                         df_diff=mes03.df_diff,
+                                         df_diff=mes0n.df_diff,
                                          ignore_zernike_number_list=[1, 2, 3, 4, 5, 6, 7, 8, 11])
 
     fig1 = plt.figure(figsize=(7, 7))
     gs1 = fig1.add_gridspec(2, 1)
 
     ax11 = fig1.add_subplot(gs1[0, 0])
-    ax11.plot(res03.df_diff["degree"], res03.df_diff["height"], label="height_diff")
-    ax11.plot(res03.df_diff["degree"], res03.removing_zernike, label="zernike_fitting")
+    ax11.plot(res0n.df_diff["degree"], res0n.df_diff["height"], label="height_transform")
+    ax11.plot(res0n.df_diff["degree"], res0n.removing_zernike, label="zernike_fitting")
     ax11.grid()
     ax11.legend()
 
     ax12 = fig1.add_subplot(gs1[1, 0])
-    ax12.plot(res03.df_diff["degree"], res03.height_removed)
+    ax12.plot(res0n.df_diff["degree"], res0n.height_removed)
     ax12.grid()
     ax12.set_xlabel("robot-arm theta [deg]")
 
