8309a640fd9ad4c5f73a451552cbaccf6e2bbad8
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-02-06 14:42:47 +0900

    研磨no8後の測定データに対してwh最適化

--- pom_wh_optimize.py
+++ pom_wh_optimize.py
@@ -11,20 +11,27 @@ import numpy as np
 import planets_optimize_myclass as pom
 
 if __name__ == "__main__":
-    CONSTS = pom.Constants(physical_radius=925e-3,
-                           ignore_radius=25e-3,
-                           pixel_number=256,
-                           zernike_max_degree=10,
-                           offset_height_percent=2)
+    CONSTS = pom.Constants(
+        physical_radius=925e-3,
+        ignore_radius=25e-3,
+        pixel_number=256,
+        zernike_max_degree=10,
+        offset_height_percent=2)
 
-    target_surface = pom.ZernikeToSurface(constants=CONSTS,
-                                          zernike_number_list=[7],
-                                          zernike_value_array=np.array([2e-7]))
+    target_surface = pom.StitchedCsvToSurface(
+        constants=CONSTS,
+        original_stitched_csv_fpath="mkfolder/stitch2mesh/zer03_0131xm130allcc_0201cirAll.v4.8.hei_dense.csv",
+        deformed_stitched_csv_fpath="")
+
+    zernike_removed_surface = pom.ZernikeRemovedSurface(
+        constants=CONSTS,
+        inputed_surface=target_surface.surface,
+        removing_zernike_number_list=[1, 2, 3, 4, 5, 6])
 
     reproducted_torque = pom.ZernikeToTorque(
         constants=CONSTS,
-        target_zernike_number_list=target_surface.zernike_number_list,
-        target_zernike_value_array=target_surface.zernike_value_array,
+        target_zernike_number_list=np.arange(1, 11),
+        target_zernike_value_array=zernike_removed_surface.zernike_value_array,
         ignore_zernike_number_list=[1])
 
     reproducted_zernike = pom.TorqueToZernike(
@@ -38,7 +45,15 @@ if __name__ == "__main__":
         zernike_number_list=reproducted_zernike.remaining_zernike_number_list,
         zernike_value_array=reproducted_zernike.remaining_reproducted_zernike_value_array)
 
+    result_surface = pom.Surface(
+        constants=CONSTS,
+        surface=target_surface.surface - reproducted_surface.surface)
+
     reproducted_restructed_surface = pom.ZernikeToSurface(
         constants=CONSTS,
         zernike_number_list=reproducted_zernike.remaining_zernike_number_list,
         zernike_value_array=reproducted_zernike.remaining_reproducted_restructed_zernike_value_array)
+
+    result_restructed_surface = pom.Surface(
+        constants=CONSTS,
+        surface=target_surface.surface - reproducted_restructed_surface.surface)
