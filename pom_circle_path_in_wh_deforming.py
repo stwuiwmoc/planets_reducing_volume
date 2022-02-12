e30b62d5b48b166a7c885d8b6a4557407cc1ed78
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-02-12 19:35:22 +0900

    indent rainbowに怒られない書き方に変更

--- pom_circle_path_in_wh_deforming.py
+++ pom_circle_path_in_wh_deforming.py
@@ -44,47 +44,60 @@ def make_full_torque_value_array(torque_number_list, torque_value_aray):
 if __name__ == "__main__":
     importlib.reload(pom)
 
-    CONSTS = pom.Constants(physical_radius=925e-3,
-                           ignore_radius=25e-3,
-                           pixel_number=256,
-                           zernike_max_degree=10,
-                           offset_height_percent=0)
+    CONSTS = pom.Constants(
+        physical_radius=925e-3,
+        ignore_radius=25e-3,
+        pixel_number=256,
+        zernike_max_degree=11,
+        offset_height_percent=0)
     """
     # for parameter study
-    original_torque_value_array = make_full_torque_value_array([1, 7, 13, 19, 25, 31],
-                                                               [5, -5, 5, -5, 5, -5])
-    original_torque_value_array = make_full_torque_value_array([2, 8, 14, 20, 26, 32],
-                                                               [5, -5, 5, -5, 5, -5])
-    original_torque_value_array = make_full_torque_value_array([3, 9, 15, 21, 27, 33],
-                                                               [5, -5, 5, -5, 5, -5])
-    original_torque_value_array = make_full_torque_value_array([4, 10, 16, 22, 28, 34],
-                                                               [5, -5, 5, -5, 5, -5])
-    original_torque_value_array = make_full_torque_value_array([5, 11, 17, 23, 29, 35],
-                                                               [5, -5, 5, -5, 5, -5])
-    original_torque_value_array = make_full_torque_value_array([6, 12, 18, 24, 30, 36],
-                                                               [5, -5, 5, -5, 5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [1, 7, 13, 19, 25, 31],
+        [5, -5, 5, -5, 5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [2, 8, 14, 20, 26, 32],
+        [5, -5, 5, -5, 5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [3, 9, 15, 21, 27, 33],
+        [5, -5, 5, -5, 5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [4, 10, 16, 22, 28, 34],
+        [5, -5, 5, -5, 5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [5, 11, 17, 23, 29, 35],
+        [5, -5, 5, -5, 5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [6, 12, 18, 24, 30, 36],
+        [5, -5, 5, -5, 5, -5])
 
-    original_torque_value_array = make_full_torque_value_array([4, 10, 15, 21, 28, 34],
-                                                               [5, -5, 5, -5, 5, -5])
-    original_torque_value_array = make_full_torque_value_array([15, 21],
-                                                               [5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [4, 10, 15, 21, 28, 34],
+        [5, -5, 5, -5, 5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [15, 21],
+        [5, -5])
 
 
-    original_torque_value_array = make_full_torque_value_array([1, 7, 13, 19, 25, 31, 3, 9, 15, 21, 27, 33, 4, 10, 16, 22, 28, 34, 5, 11, 17, 23, 29, 35, 6, 12, 18, 24, 30, 36],
-                                                               [-5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, 5, -5, 5, -5, 5, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [1, 7, 13, 19, 25, 31, 3, 9, 15, 21, 27, 33, 4, 10, 16, 22, 28, 34, 5, 11, 17, 23, 29, 35, 6, 12, 18, 24, 30, 36],
+        [-5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, -5, 5, 5, -5, 5, -5, 5, -5])
     """
 
-    original_torque_value_array = make_full_torque_value_array([6, 12, 18, 24, 30, 36],
-                                                               [0, 5, 0, -5, 0, -5])
+    original_torque_value_array = make_full_torque_value_array(
+        [6, 12, 18, 24, 30, 36],
+        [0, 5, 0, -5, 0, -5])
 
-    wh_deformed_zernike = pom.TorqueToZernike(constants=CONSTS,
-                                              torque_value_array=original_torque_value_array,
-                                              restructed_torque_value=5,
-                                              ignore_zernike_number_list=[1, 2, 3, 4, 5, 6])
+    wh_deformed_zernike = pom.TorqueToZernike(
+        constants=CONSTS,
+        torque_value_array=original_torque_value_array,
+        restructed_torque_value=5,
+        ignore_zernike_number_list=[1, 2, 3, 4, 5, 6])
 
-    wh_deformed_surface = pom.ZernikeToSurface(constants=CONSTS,
-                                               zernike_number_list=wh_deformed_zernike.remaining_zernike_number_list,
-                                               zernike_value_array=wh_deformed_zernike.remaining_reproducted_zernike_value_array)
+    wh_deformed_surface = pom.ZernikeToSurface(
+        constants=CONSTS,
+        zernike_number_list=wh_deformed_zernike.remaining_zernike_number_list,
+        zernike_value_array=wh_deformed_zernike.remaining_reproducted_zernike_value_array)
 
     fig = plt.figure(figsize=(15, 7))
     gs = fig.add_gridspec(2, 3)
