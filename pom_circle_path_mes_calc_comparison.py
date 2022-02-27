aec30ecebecf9a4e87715990581d1b1a191d708c
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-02-27 18:48:42 +0900

    femの重ね合わせの解析を追加

--- pom_circle_path_mes_calc_comparison.py
+++ pom_circle_path_mes_calc_comparison.py
@@ -91,6 +91,26 @@ if __name__ == "__main__":
         inputed_surface=omx_deformed_surface.surface,
         removing_zernike_number_list=ignore_zernike_number_list)
 
+    # fem calculate
+    fem_surface_array = np.zeros((CONSTS.pixel_number, CONSTS.pixel_number))
+    for i in range(len(torque_number_list)):
+        fem_single_surface = pom.FemTxtToSurface(
+            constants=CONSTS,
+            wt_version=6,
+            wt_number=torque_number_list[i])
+
+        fem_surface_array += fem_single_surface.surface * torque_value_array[i]
+        del fem_single_surface
+
+    fem_composited_surface = pom.Surface(
+        constants=CONSTS,
+        surface=fem_surface_array)
+
+    fem_composited_zernike_removed_surface = pom.ZernikeRemovedSurface(
+        constants=CONSTS,
+        inputed_surface=fem_composited_surface.surface,
+        removing_zernike_number_list=[1, 2, 3, 4])
+
     # plot
 
     fig1 = plt.figure(figsize=(7, 10))
@@ -98,6 +118,7 @@ if __name__ == "__main__":
 
     ax11 = omx_deformed_zernike_removed_surface.make_image_plot(figure=fig1, position=gs1[0:2, :])
     ax11 = omx_deformed_zernike_removed_surface.make_torque_fulcrum_plot(ax=ax11, torque_value_array=original_torque_value_array)
+    ax11.set_title("operation matrix model\n" + ax11.get_title())
 
     ax12 = omx_deformed_zernike_removed_surface.make_circle_path_plot(figure=fig1, position=gs1[2, :], line_label="model")
     ax12.plot(mes_zer0n.df_diff["degree"], mes_zer0n.height_removed * 1e9, label="measurement " + serials[ptn])
@@ -113,3 +134,25 @@ if __name__ == "__main__":
 
     fig1.tight_layout()
     fig1.savefig(mkfolder() + serials[ptn] + "_fig1.png")
+
+    fig2 = plt.figure(figsize=(7, 10))
+    gs2 = fig2.add_gridspec(3, 2)
+
+    ax21 = fem_composited_zernike_removed_surface.make_image_plot(figure=fig2, position=gs2[0:2, :])
+    ax21 = fem_composited_zernike_removed_surface.make_torque_fulcrum_plot(ax=ax21, torque_value_array=original_torque_value_array)
+    ax21.set_title("FEM model\n" + ax21.get_title())
+
+    ax22 = fem_composited_zernike_removed_surface.make_circle_path_plot(figure=fig2, position=gs2[2, :], line_label="model")
+    ax22.plot(mes_zer0n.df_diff["degree"], mes_zer0n.height_removed * 1e9, label="measurement " + serials[ptn])
+    ax22.legend()
+
+    pv_fem = float(ax22.get_title()[19:25])
+    pv_mes = 1e9 * (mes_zer0n.height_removed.max() - mes_zer0n.height_removed.min())
+
+    ax22.set_title(
+        "measurement pv = " + str(round(pv_mes, 2)) + " [nm] / " +
+        "model pv = " + str(round(pv_fem, 2)) + " [nm]\n" +
+        "measurement / model = " + str(round(pv_mes / pv_fem, 3)))
+
+    fig2.tight_layout()
+    fig2.savefig(mkfolder() + serials[ptn] + "_fig2.png")
