fb6f6459261316f372fc8fb9aa67ef4be11c3d19
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-10-28 20:07:22 +0900

    とりあえず高さをプロット

--- test_CirclePathMeasurementTxtReading.py
+++ test_CirclePathMeasurementTxtReading.py
@@ -2,6 +2,8 @@
 
 import importlib
 
+import matplotlib.pyplot as plt
+
 import planets_optimize_myclass as pom
 
 if __name__ == "__main__":
@@ -22,3 +24,15 @@ if __name__ == "__main__":
         original_txt_fpath=original_filepath,
         deformed_txt_fpath=deformed_filepath
     )
+
+    fig1 = plt.figure()
+    gs1 = fig1.add_gridspec(1, 1)
+
+    ax11 = fig1.add_subplot(gs1[0, 0])
+    ax11.plot(diff.df_diff["degree"], diff.df_diff["height"])
+
+    ax11.set_xlabel("degree")
+    ax11.set_ylabel("height [m]")
+    ax11.grid()
+
+    fig1.tight_layout()
