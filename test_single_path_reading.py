7853e0712cedbc4c321b8169f70622a6757c0c75
stwuiwmoc <stwuiwmoc5929@gmail.com>
2022-10-28 18:59:45 +0900

    インデントが怒られないように修正

--- test_single_path_reading.py
+++ test_single_path_reading.py
@@ -28,23 +28,26 @@ def mkfolder(suffix=""):
 if __name__ == "__main__":
     importlib.reload(pom)
 
-    CONSTS = pom.Constants(physical_radius=925e-3,
-                           ignore_radius=25e-3,
-                           pixel_number=1024,
-                           zernike_max_degree=10,
-                           offset_height_percent=2)
+    CONSTS = pom.Constants(
+        physical_radius=925e-3,
+        ignore_radius=25e-3,
+        pixel_number=1024,
+        zernike_max_degree=10,
+        offset_height_percent=2)
 
     serial_n = "0117l"
     input_filename_n = "../sag_integration_code/" + "mkfolder/psm_test_kagi_data_integration/" + serial_n + "_height.csv"
 
-    mes = pom.CirclePathMeasurementCsvReading(Constants=CONSTS,
-                                              original_csv_fpath=input_filename_n,
-                                              deformed_csv_fpath="")
+    mes = pom.CirclePathMeasurementCsvReading(
+        Constants=CONSTS,
+        original_csv_fpath=input_filename_n,
+        deformed_csv_fpath="")
 
-    res = pom.CirclePathZernikeFitting(Constants=CONSTS,
-                                       circle_path_radius=870e-3,
-                                       df_diff=mes.df_diff,
-                                       ignore_zernike_number_list=[1, 2, 3, 4, 5, 6, 7, 8, 11])
+    res = pom.CirclePathZernikeFitting(
+        Constants=CONSTS,
+        circle_path_radius=870e-3,
+        df_diff=mes.df_diff,
+        ignore_zernike_number_list=[1, 2, 3, 4, 5, 6, 7, 8, 11])
 
     circumference = -res.df_diff["radian"] * res.circle_path_radius * 1e3
 
