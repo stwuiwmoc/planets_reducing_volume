3ca25c1d422b434f04dd305fb0fcc399034a730b
stwuiwmoc <stwuiwmoc5929@gmail.com>
2023-01-07 23:52:14 +0900

    xとyの対応表も作成

--- make_latex_matrix_text.py
+++ make_latex_matrix_text.py
@@ -120,3 +120,38 @@ if __name__ == "__main__":
             print(r"    ", end="")
             print(r"y _{" + str(i) + r"}", end="")
             print(r" \\", end="\n")
+
+    # %%
+    x_index_list = [
+        1, None, 5, 6, 7, None, 11,
+        12, None, 16, 17, 18, None, 22,
+        23, None, 27, 28, 29, None, 33
+    ]
+    y_index_list = [
+        1, None, 5, 6, 7, None, 11,
+        13, None, 17, 18, 19, None, 23,
+        25, None, 29, 30, 31, None, 35
+    ]
+
+    for i in x_index_list:
+        if i is None:
+            print(r"| $\cdots$ ", end="")
+
+        else:
+            print(r"| $x _{" + str(i) + r"}$ ", end="")
+    print("|")
+
+    for i in range(len(x_index_list)):
+        print(r"| - ", end="")
+    print("|")
+
+    for i in y_index_list:
+        if i is None:
+            print(r"| $\cdots$ ", end="")
+
+        elif i in [6, 18, 30]:
+            print(r"| $y _{" + str(i) + r"} + y _{" + str(i + 6) + r"}$ ", end="")
+
+        else:
+            print(r"| $y _{" + str(i) + r"}$ ", end="")
+    print("|")
